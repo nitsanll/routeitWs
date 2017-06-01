b491dfab1b6d89ec6f7a26bce12abfd09d063e10
Nitsan Levin <Nitsan Levin>
2017-06-01 16:01:28 +0300

    changes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set('port', port);
 app.use('/',express.static('./public'));
 app.use(function(req,res,next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     app.set('json spaces', 4);
     res.set("Content-Type", "application/json");
