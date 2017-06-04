5fb22f7845925b7f1bc20da58057901b707334e0
Nitsan Levin <Nitsan Levin>
2017-06-04 12:34:08 +0300

    changes

--- travelersController.js
+++ travelersController.js
@@ -106,10 +106,9 @@ exports.updateTripDates = function(mail, tripId, sDate, daysNum, isFri, isSat, c
   //console.log("first " + sDate);
   //calculate trip's end date
   //console.log(sDate.getDate());
-  var eDate = new Date();
-  eDate.setDate(sDate.getDate());
+  var eDate = new Date(sDate);
   //eDate.toUTCString();
-  //eDate.setDate(eDate.getDate()+parseInt(daysNum-1));
+  eDate.setDate(eDate.getUTCDate()+parseInt(daysNum-1));
   callback({"sDate": sDate, "eDate": eDate});
   //console.log("start date: " +sDate);
   //console.log("end date: "+eDate +"\n");
