3e669960df0a4188c3c76dbf8ed3f82060e641df
Nitsan Levin <Nitsan Levin>
2017-06-04 12:42:59 +0300

    changes

--- travelersController.js
+++ travelersController.js
@@ -102,7 +102,7 @@ exports.addRouteToTraveler = function(id, mail, callback){
 
 // calculate trip end date and daily sections dates and update the trip dates 
 exports.updateTripDates = function(mail, tripId, sDate, daysNum, isFri, isSat, callback){	
-  //var sDate = new Date(sDate1);
+  sDate.setHours(14);
   //console.log("first " + sDate);
   //calculate trip's end date
   //console.log(sDate.getDate());
